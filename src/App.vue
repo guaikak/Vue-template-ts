d48dc03dba045e64485c43618e458de9f736a699
yg <3280565352@qq.com>
2024-12-06 19:27:56 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div id="App">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+html,
+body,
+div#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+let router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    // 路径 path
+    // 模板 component
+    
+  ],
+});
+
+export default router;
